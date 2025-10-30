790478f7fb12e5ff3904bcda1f583f471d12ca93
Pratyush Nishank <pratyush.deve@gmail.com>
2025-10-30 23:09:13 +0530

    Corrected Textfield, Set alignments for all the textfields, added UI changes to Top bar and navigation bar

--- app/src/main/java/com/example/ui_design/MainActivity.kt
+++ app/src/main/java/com/example/ui_design/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,11 +16,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.BottomAppBar
@@ -29,12 +34,14 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -70,12 +77,40 @@ fun Screen(){
         topBar = {
             TopAppBar(
                 modifier = Modifier
-                    .fillMaxWidth(),
+                    .fillMaxWidth()
+                    .statusBarsPadding(),
                 title = {
-                    Text(
-                        text = "HALA MADRID",
-                        textAlign = TextAlign.Center,
-                        fontWeight = FontWeight.Bold,
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(
+                                color = Color.Blue
+                            ),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center
+                    ){
+                        Text(
+                            text = "HALA MADRID",
+                            textAlign = TextAlign.Center,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.Black
+                        )
+                    }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Blue
+                ),
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Home",
+                        modifier = Modifier
+                            .clickable(
+                                onClick = {
+                                    /*todo*/
+                                }
+                            ),
+                        tint = Color.Black
                     )
                 }
             )
@@ -83,104 +118,136 @@ fun Screen(){
         bottomBar = {
                 NavigationBar(
                         modifier = Modifier
-                            .fillMaxWidth()
+                            .fillMaxWidth(),
+                    containerColor = Color.Blue
                 ) {
-                    Row(
+                    Spacer(modifier = Modifier.width(6.dp))
+                    Icon(
+                        imageVector = Icons.Filled.Home,
+                        contentDescription = "Home",
                         modifier = Modifier
-                            .fillMaxWidth()
-                    ){
-                        Icon(
-                            imageVector = Icons.Default.Home,
-                            contentDescription = "Home",
-                            modifier = Modifier
-                                .clickable(
-                                    onClick = {/*todo*/}
-                                )
-                        )
-                        Spacer(modifier = Modifier.weight(1f))
-                        Icon(
-                            imageVector = Icons.Default.Person,
-                            contentDescription = "Home",
-                            modifier = Modifier
-                                .clickable(
-                                    onClick = {/*todo*/}
-                                )
-                        )
-                    }
+                            .clickable(
+                                onClick = {/*todo*/}
+                            )
+                            .padding(vertical = 8.dp)
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Icon(
+                        imageVector = Icons.Filled.Person,
+                        contentDescription = "Home",
+                        modifier = Modifier
+                            .clickable(
+                                onClick = {/*todo*/}
+                            )
+                            .padding(vertical = 8.dp)
+                    )
+                    Spacer(modifier = Modifier.width(6.dp))
                 }
-
         }
     ) { padding->
         var name by remember { mutableStateOf("") }
         var rollNo by remember { mutableStateOf("") }
+        var section by remember { mutableStateOf("") }
+        var club by remember { mutableStateOf("") }
         Column(
             modifier = Modifier
                 .padding(padding)
         ) {
+            Spacer(modifier = Modifier.height(15.dp))
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(50.dp)
+                    .padding(top = 10.dp)
+                    .height(300.dp),
+                contentAlignment = Alignment.Center
             ){
                 Image(
                     painter = painterResource(id = R.drawable.img),
                     contentDescription = "Avatar icon",
                     contentScale = ContentScale.Crop,
                     modifier = Modifier
-                        .width(50.dp)
-                        .height(50.dp)
+                        .width(200.dp)
+                        .height(200.dp)
                         .clip(CircleShape)
-                        .border(2.dp, Color.Cyan, CircleShape)
+                        .border(2.dp, Color.Cyan, CircleShape),
                 )
             }
-            Spacer(modifier = Modifier.height(4.dp))
+            Spacer(modifier = Modifier.height(20.dp))
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .padding(horizontal = 40.dp)
             ){
                 OutlinedTextField(
-                    placeholder = Text("Name"),
+                    placeholder = {
+                        Text(
+                            "Name",
+                            fontWeight = FontWeight.Bold
+                        )
+                    },
                     value = name,
-                    onValueChange = {name.it },
+                    onValueChange = {name = it },
                     modifier = Modifier
                         .fillMaxWidth(),
                     shape = RoundedCornerShape(12.dp)
                 )
             }
-            Spacer(modifier = Modifier.height(4.dp))
+            Spacer(modifier = Modifier.height(12.dp))
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .padding(horizontal = 40.dp)
             ){
                 OutlinedTextField(
-                    label = "Roll no",
+                    label = {
+                        Text(
+                            "Roll No",
+                            fontWeight = FontWeight.Bold
+                        )
+                    },
+                    placeholder = {
+                        Text("Eg: 211441")
+                    },
                     value = rollNo,
-                    onValueChange = {rollNo},
-                    placeholder = Text("eg. 124112"),
+                    onValueChange = {rollNo = it },
                     modifier = Modifier
                         .fillMaxWidth(),
-                    shape = RoundedCornerShape(12.dp)
-                )
+                    shape = RoundedCornerShape(12.dp))
             }
-            Spacer(modifier = Modifier.height(4.dp))
+            Spacer(modifier = Modifier.height(12.dp))
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .padding(horizontal = 40.dp)
             ){
                 OutlinedTextField(
-                    placeholder = Text("Section"),
+                    placeholder = {
+                        Text(
+                            "Section",
+                            fontWeight = FontWeight.Bold
+                        )
+                    },
+                    value = section,
+                    onValueChange = { section = it },
                     modifier = Modifier
                         .fillMaxWidth(),
-                    shape = RoundedCornerShape(12.dp)
-                )
+                    shape = RoundedCornerShape(12.dp))
             }
-            Spacer(modifier = Modifier.height(4.dp))
+            Spacer(modifier = Modifier.height(12.dp))
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .padding(horizontal = 40.dp)
             ){
                 OutlinedTextField(
-                    placeholder = Text("Favoutarite Club"),
+                    placeholder = {
+                        Text(
+                            "Favourite Club",
+                            fontWeight = FontWeight.Bold
+                        )
+                    },
+                    value = club,
+                    onValueChange = { club = it },
                     modifier = Modifier
                         .fillMaxWidth(),
                     shape = RoundedCornerShape(12.dp)
